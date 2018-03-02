301b2915588dc425fd87c20e7150a66141992db6
shivatarun123 <bstarun123@gmail.com>
2018-03-02 15:49:06 +0100

    Create Switch.java

---
+++ If-Else Statemet/Switch.java
@@ -0,0 +1,18 @@
+package com.Switch;
+
+
+public class Switch {
+    public static void main(String[] args){
+     int num =2;
+
+     switch(num){
+         case 1: System.out.println("The number is : "+num);
+         break;
+         case 2: System.out.println("The  number is : "+num);
+         break;
+         case 3: System.out.println("The number is : "+num);
+         break;
+         default: System.out.println("new number is : "+num);
+     }
+    }
+}

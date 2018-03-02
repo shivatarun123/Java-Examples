f8e051c861c3d6267d33bc74e10dc3dda6e1ff76
shivatarun123 <bstarun123@gmail.com>
2018-03-02 15:29:34 +0100

    Inheritance and method override Example

---
+++ Inheritance/Main.java
@@ -0,0 +1,10 @@
+package Inheritence;
+
+public class Main {
+public static void main(String[] args){
+Car ferrari  = new Car("ferrari","car",2009,"black",4,5);
+    System.out.println(ferrari.wheels);
+    System.out.println(ferrari.name);
+    System.out.println(ferrari.getWheels());
+}
+}

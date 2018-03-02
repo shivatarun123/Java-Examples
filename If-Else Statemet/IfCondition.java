ab61d9998f5c0d968191ea39613edbce53aed329
shivatarun123 <bstarun123@gmail.com>
2018-03-02 15:40:47 +0100

    Create IfCondition.java

---
+++ If-Else Statemet/IfCondition.java
@@ -0,0 +1,13 @@
+package com.ifpackage;
+
+
+public class IfCondition {
+
+    public static void main(String args[]){
+        int a=100;
+        int  b=20;
+        if(a>b){
+           System.out.println(a+" is greater than "+b);
+        }
+    }
+}

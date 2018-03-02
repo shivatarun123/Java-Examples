31c66f8a18709785ae53d4b167052738ba0ae2db
shivatarun123 <bstarun123@gmail.com>
2018-03-02 15:32:29 +0100

    Create ForLoop.java

---
+++ Loops/ForLoop.java
@@ -0,0 +1,14 @@
+package com.Loops;
+
+
+public class BasicForLoop {
+
+    public static void main(String srgs[]){
+
+        for(int i=1;i<=5;i++){
+            System.out.println("This is row :"+i);
+        }
+
+    }
+
+}

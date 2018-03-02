85edc9cd9638f2045abebfa263b64278a0645eb4
shivatarun123 <bstarun123@gmail.com>
2018-03-02 15:33:33 +0100

    Create WhileLoop.java

---
+++ Loops/WhileLoop.java
@@ -0,0 +1,15 @@
+ackage com.Loops;
+
+
+public class WhileLoop {
+
+    public static void main(String srgs[]){
+        int i=1;
+        while(i<=6){
+            System.out.println("This is row :"+i);
+             i++;
+        }
+
+    }
+
+}

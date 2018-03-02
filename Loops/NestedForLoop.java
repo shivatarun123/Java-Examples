e9e1d7d434f6db1afc6b8676d826dd46fe73c5b4
shivatarun123 <bstarun123@gmail.com>
2018-03-02 15:34:26 +0100

    Create NestedForLoop.java

---
+++ Loops/NestedForLoop.java
@@ -0,0 +1,17 @@
+package com.Loops;
+
+
+public class NestedForLoop {
+
+    public static void main(String srgs[]){
+
+        for(int i=1;i<=10;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+
+    }
+
+}
